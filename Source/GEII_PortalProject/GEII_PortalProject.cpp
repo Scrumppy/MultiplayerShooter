c139d177c5531519c92a739984e027f8c556a774
Scrumppy <42845422+Scrumppy@users.noreply.github.com>
2022-09-11 14:52:54 +0100

    Folder Organization

--- Source/GEII_PortalProject/GEII_PortalProject.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GEII_PortalProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GEII_PortalProject, "GEII_PortalProject" );
- 
